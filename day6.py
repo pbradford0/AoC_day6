427e1646f5f6950d34edc27cc84749ebb6958438
pbradford0 <phil.w.bradford@gmail.com>
2016-11-07 20:23:08 -0500

    omg, it works

--- day6.py
+++ day6.py
@@ -12,14 +12,14 @@ def lights_toggle(low_left, top_right, *light_grid):
   grid_y = int(snip_bottom[1])
   while grid_x <= int(snip_top[0]):
     while grid_y <= int(snip_top[1]):
-      if light_grid[grid_x[grid_y]] == 1:
-        light_grid[grid_x[grid_y]] = 0
+      if light_grid[grid_x][grid_y] == 1:
+        light_grid[grid_x][grid_y] = 0
       else:
-       light_grid[grid_x[grid_y]] = 1
+       light_grid[grid_x][grid_y] = 1
       grid_y = grid_y + 1
     grid_y = int(snip_bottom[1])
     grid_x = grid_x + 1
-  return
+  return light_grid
 
 def lights_off(low_left, top_right, *light_grid):
   #no output, just change a grid (dict) and set a range = 0 in each cell
@@ -30,11 +30,11 @@ def lights_off(low_left, top_right, *light_grid):
   grid_y = int(snip_bottom[1])
   while grid_x <= int(snip_top[0]):
     while grid_y <= int(snip_top[1]):
-      light_grid[grid_x[grid_y]] = 0
+      light_grid[grid_x][grid_y] = 0
       grid_y = grid_y + 1
     grid_y = int(snip_bottom[1])
     grid_x = grid_x + 1
-  return
+  return light_grid
 
 def lights_on(low_left, top_right, *light_grid):
   #no output, just change a grid (dict) and set a range = 1 in each cell
@@ -45,11 +45,11 @@ def lights_on(low_left, top_right, *light_grid):
   grid_y = int(snip_bottom[1])
   while grid_x <= int(snip_top[0]):
     while grid_y <= int(snip_top[1]):
-      light_grid[grid_x[grid_y]] = 1
+      light_grid[grid_x][grid_y] = 1
       grid_y = grid_y + 1
     grid_y = 0
     grid_x = grid_x + 1
-  return
+  return light_grid
 
 def count_lights(grid):
   #output: number of lights currently lit
@@ -59,7 +59,7 @@ def count_lights(grid):
   grid_y = 0
   while grid_x <= 999:
     while grid_y <= 999:
-      if light_grid[grid_x[grid_y]] == 1:
+      if grid[grid_x][grid_y] == 1:
         lit = lit + 1
       grid_y = grid_y + 1
     grid_y = 0
@@ -86,7 +86,7 @@ def defeat_neighbor(filename):
      #separate the command into words, then use the first 2 to determine action
     snip = command.split()
     if snip[0] == "toggle":
-      light_grid = toggle(snip[1], snip[3], *light_grid)
+      light_grid = lights_toggle(snip[1], snip[3], *light_grid)
     elif snip[0] == "turn":
       if snip[1] == "on":
         light_grid = lights_on(snip[2], snip[4], *light_grid)
